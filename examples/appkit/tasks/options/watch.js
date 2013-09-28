151b2a9d83d57918c47d9037c466c46d34c4068c
Taras Mankovski <tarasm@gmail.com>
2013-09-28 08:07:03 +0200

    Added watcher patches for templates and contents

--- examples/appkit/tasks/options/watch.js
+++ examples/appkit/tasks/options/watch.js
@@ -1,10 +1,10 @@
 module.exports = {
   main: {
-    files: ['app/**/*', 'public/**/*', 'vendor/**/*', 'tests/**/*'],
+    files: ['app/**/*', 'public/**/*', 'vendor/**/*', 'tests/**/*', 'contents/**/*', 'templates/**/*'],
     tasks: ['build:debug']
   },
   test: {
-    files: ['app/**/*', 'public/**/*', 'vendor/**/*', 'tests/**/*'],
+    files: ['app/**/*', 'public/**/*', 'vendor/**/*', 'tests/**/*', 'contents/**/*', 'templates/**/*'],
     tasks: ['build:debug', 'karma:server:run']
   },
   options: {
